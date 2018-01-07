713c0b82e2e6273d2262a5356cbf5f8d949a90b8
zyf429018905 <429018905@qq.com>
2018-01-07 17:37:25 +0800

    =====

--- index.php
+++ index.php
@@ -1,20 +1,5 @@
 <?php
-header("Content-Type:text/html; charset=utf-8");
-$host = $_SERVER['HTTP_HOST'];
-define("SITE_ID", 2017);
-define("SITE_TYPE", 1);
-define("IMG_SITE_ID", 49);
-define("DS_SS_HANDICP", 'B');
-define("DS_XG_HANDICP", 'C');
-define("MY_HTTP_HOST", 'http://api.hdc100.ph/api.php');
-define("MY_HTTP_HOST_URL", 'http://api.hdc100.ph');
-define("NEW_KEYB", '11_2');
-
-if (function_exists('date_default_timezone_set')) {
-    @date_default_timezone_set('Etc/GMT-8');
-}
 require_once($_SERVER['DOCUMENT_ROOT']."/src/Hprose.php");
-
 use Hprose\Http\Server;
 
 function getNotice($params){
@@ -31,7 +16,7 @@ $server->start();
 //$params['note_type'] = "1";
 //$re = $clientA->getNotice($params);
 //echo json_encode($re);die;
-
+?>
 
 
 
