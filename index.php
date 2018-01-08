25c1050dcf984c27b298af9a25b803fd1553cdf3
zyf429018905 <429018905@qq.com>
2018-01-08 14:31:59 +0800

    =====

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <?php
-require_once(__DIR__."/src/Hprose.php");
+//require_once(__DIR__."/src/Hprose.php");
 use Hprose\Http\Server;
-function getNotice($params){
-    $re['param'] = $params;
-    $re['code'] = "10000";
-    return json_encode($re);
+//function getNotice($params){
+//    $re['param'] = $params;
+//    $re['code'] = "10000";
+//    return json_encode($re);
+//}
+function hello($name) {
+    return "Hello $name!";
 }
 $server = new Server();
-$server->addFunction('getNotice');
+$server->addFunction('hello');
 $server->start();
 ?>
 
