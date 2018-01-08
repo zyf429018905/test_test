39571ecfea075d02c58ca19798295cfcb1ebff78
zyf429018905 <429018905@qq.com>
2018-01-08 12:19:19 +0800

    =====

--- index.php
+++ index.php
@@ -5,17 +5,13 @@ use Hprose\Http\Server;
 function getNotice($params){
     $re['param'] = $params;
     $re['code'] = "10000";
-    return json_encode($re);
+    echo json_encode($re);
 }
 
 $server = new Server();
 $server->addFunction('getNotice');
 $server->start();
 
-//$params['company'] = SITE_ID;
-//$params['note_type'] = "1";
-//$re = $clientA->getNotice($params);
-//echo json_encode($re);die;
 ?>
 
 
